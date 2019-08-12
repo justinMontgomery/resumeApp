dd2ecf6f6002c4ed0758fb77daa0f0b442e53264
Justin <justin.montgomery2@gmail.com>
2019-08-12 16:42:40 -0400

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import React from 'react';
-import logo from './logo.svg';
+import logo from './logo.jpg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
+        <h1>Justin Montgomery</h1>
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
