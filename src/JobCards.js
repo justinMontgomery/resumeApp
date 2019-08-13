e0b4ced1e7a6da4efd987c1a64de7669a2855c93
Justin <justin@montgomery.run>
2019-08-12 23:23:34 -0400

    move job cards to file

---
+++ src/JobCards.js
@@ -0,0 +1,32 @@
+import React, { useState } from 'react';
+import Card from 'react-bootstrap/Card';
+import Collapse from 'react-bootstrap/Collapse';
+
+
+function JobCards(props) {
+    const [open, setOpen] = useState(false);
+    return (
+        <>
+            <Card>
+                <Card.Header onClick={() => setOpen(!open)}>
+                    <button class="btn btn-link" type="button">{props.name}</button>
+                </Card.Header>
+                <Collapse in={open}>
+                    <div>
+                        <Card.Body>
+                            <Card.Title>
+                                {props.company}
+                            </Card.Title>
+                            <Card.Subtitle>{props.timeFrame}</Card.Subtitle>
+                            <Card.Text>
+                                {props.list}
+                            </Card.Text>
+                        </Card.Body>
+                    </div>
+                </Collapse>
+            </Card>
+        </>
+    );
+}
+
+export default JobCards;
