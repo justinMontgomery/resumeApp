4c59c5930cf1f8679e446f6e8ef49e472cdaef4a
Justin <justin@montgomery.run>
2020-09-11 22:49:54 -0400

    update packages and format some files

--- src/App.js
+++ src/App.js
@@ -1,42 +1,54 @@
-import React, { Component } from 'react';
-import logo from './logo.jpg';
-import './App.css';
+import React, { Component } from "react";
+import logo from "./logo.jpg";
+import "./App.css";
 import "../node_modules/@fortawesome/fontawesome-free/js/all.js";
 import "../node_modules/@fortawesome/fontawesome-free/css/all.css";
-import JobCards from './JobCards';
+import JobCards from "./JobCards";
 
 function Paycor() {
-  return (
-    <a href="https://www.paycor.com">Paycor</a>
-  );
+  return <a href="https://www.paycor.com">Paycor</a>;
 }
 
 function Sundries() {
-  return (
-    <a href="http://www.sundriessolutions.com/">Sundries Solutions</a>
-  );
+  return <a href="http://www.sundriessolutions.com/">Sundries Solutions</a>;
 }
 
 function OU() {
-  return (
-    <a href="https://www.ohio.edu/">Ohio University</a>
-  );
+  return <a href="https://www.ohio.edu/">Ohio University</a>;
 }
 
 function EdMap() {
-  return ("EdMap");
+  return "EdMap";
 }
 
 function SSDList() {
   return (
     <ul>
-      <li>Worked with newly formed team in Kanban environment to rewrite legacy Powerbuilder/MSSQL application in .NET Core/AzureSQL.</li>
-      <li>App managed financial transactions between Paycor, clients, employees, and various tax agencies.</li>
-      <li>Main application utilized primarily Azure based assets. Including Functions, Storage, SQL.</li>
-      <li>Extensive testing utilizing NUnit to ensure output of rewrite matched output of legacy app.</li>
-      <li>Worked with stakeholders with continual feedback to gather requirements and ensure quality and features.</li>
+      <li>
+        Worked with newly formed team in Kanban environment to rewrite legacy
+        Powerbuilder/MSSQL application in .NET Core/AzureSQL.
+      </li>
+      <li>
+        App managed financial transactions between Paycor, clients, employees,
+        and various tax agencies.
+      </li>
+      <li>
+        Main application utilized primarily Azure based assets. Including
+        Functions, Storage, SQL.
+      </li>
+      <li>
+        Extensive testing utilizing NUnit to ensure output of rewrite matched
+        output of legacy app.
+      </li>
+      <li>
+        Worked with stakeholders with continual feedback to gather requirements
+        and ensure quality and features.
+      </li>
       <li>UI in react with an API written in .NET Core hosted in Azure.</li>
-      <li>Interfaced with other legacy systems to ensure hooks which legacy app calls were present in new application.</li>
+      <li>
+        Interfaced with other legacy systems to ensure hooks which legacy app
+        calls were present in new application.
+      </li>
     </ul>
   );
 }
@@ -44,14 +56,33 @@ function SSDList() {
 function SD2List() {
   return (
     <ul>
-      <li>Worked on a variety of applications mainly centering around HR and Payroll.</li>
-      <li>Worked with a number of WebAPI .NET Framework projects with MSSQL backends interfaced with EntityFramework, Stored Procedures, ADO.NET and others.</li>
-      <li>Extensive work in MSSQL using stored procedures, triggers, user defined types, indexes, and other tools.</li>
-      <li>Monitored performance using tools like NewRelic and logging (Stackify and server logs).</li>
+      <li>
+        Worked on a variety of applications mainly centering around HR and
+        Payroll.
+      </li>
+      <li>
+        Worked with a number of WebAPI .NET Framework projects with MSSQL
+        backends interfaced with EntityFramework, Stored Procedures, ADO.NET and
+        others.
+      </li>
+      <li>
+        Extensive work in MSSQL using stored procedures, triggers, user defined
+        types, indexes, and other tools.
+      </li>
+      <li>
+        Monitored performance using tools like NewRelic and logging (Stackify
+        and server logs).
+      </li>
       <li>Unit tested features using MSTest and NUnit.</li>
-      <li>Supported user facing applications with bug fixes and feature work while working closely with involved stakeholders.</li>
+      <li>
+        Supported user facing applications with bug fixes and feature work while
+        working closely with involved stakeholders.
+      </li>
       <li>Worked with team across multiple timezones and locations.</li>
-      <li>Managed Scrum Master duties including, leading standup, retros, sprint planning, cross team dependencies, and other various responsibilities.</li>
+      <li>
+        Managed Scrum Master duties including, leading standup, retros, sprint
+        planning, cross team dependencies, and other various responsibilities.
+      </li>
     </ul>
   );
 }
@@ -59,10 +90,22 @@ function SD2List() {
 function SD1List() {
   return (
     <ul>
-      <li>Primary work centered around HR application which integrated with separate Payroll solution.</li>
-      <li>Answered support tickets and bug fixes around HR application and it's integration with Payroll solution.</li>
-      <li>Worked closely with HR specialists to quickly resolve issues and questions as they arose in their work.</li>
-      <li>Developed and deployed integration with third party Application Tracking System.</li>
+      <li>
+        Primary work centered around HR application which integrated with
+        separate Payroll solution.
+      </li>
+      <li>
+        Answered support tickets and bug fixes around HR application and it's
+        integration with Payroll solution.
+      </li>
+      <li>
+        Worked closely with HR specialists to quickly resolve issues and
+        questions as they arose in their work.
+      </li>
+      <li>
+        Developed and deployed integration with third party Application Tracking
+        System.
+      </li>
       <li>Worked with team across multiple timezones and locations.</li>
     </ul>
   );
@@ -72,7 +115,10 @@ function QAAList() {
   return (
     <ul>
       <li>Continued QA work I had previously been doing with ED MAP.</li>
-      <li>Worked closely with Ryan (Principal Consultant) for other miscellaneous tasks.</li>
+      <li>
+        Worked closely with Ryan (Principal Consultant) for other miscellaneous
+        tasks.
+      </li>
     </ul>
   );
 }
@@ -80,7 +126,10 @@ function QAAList() {
 function TAList() {
   return (
     <ul>
-      <li>Led evening help sessions to assist students through various levels of business programming.</li>
+      <li>
+        Led evening help sessions to assist students through various levels of
+        business programming.
+      </li>
     </ul>
   );
 }
@@ -93,7 +142,6 @@ function EdMapList() {
   );
 }
 export default class App extends Component {
-
   componentDidMount() {
     const script = document.createElement("script");
     script.src = "//cdn.youracclaim.com/assets/utilities/embed.js";
@@ -102,73 +150,118 @@ export default class App extends Component {
   }
 
   renderHistoryList() {
-    return <div className="resumeRow">
-      <h2>Experience</h2>
-      <div className="history">
-        <JobCards name="Senior Software Engineer" timeFrame="Sept 2018 - Present" company={Paycor()} list={SSDList()}></JobCards>
-        <JobCards name="Software Developer II" timeFrame="Nov 2012 - Sept 2018" company={Paycor()} list={SD2List()}></JobCards>
-        <JobCards name="Software Developer I" timeFrame="June 2011 - Nov 2012" company={Paycor()} list={SD1List()}></JobCards>
-        <JobCards name="Quality Assurance Analyst" timeFrame="Apr 2011 - Jun 2011" company={Sundries()} list={QAAList()}></JobCards>
-        <JobCards name="Teaching Assistant" timeFrame="Nov 2010 - Jun 2011" company={OU()} list={TAList()}></JobCards>
-        <JobCards name="Intern" timeFrame="Nov 2010 - Jun 2011" company={(EdMap())} list={EdMapList()}></JobCards>
+    return (
+      <div className="resumeRow">
+        <h2>Experience</h2>
+        <div className="history">
+          <JobCards
+            name="Senior Software Engineer"
+            timeFrame="Sept 2018 - Present"
+            company={Paycor()}
+            list={SSDList()}
+          ></JobCards>
+          <JobCards
+            name="Software Developer II"
+            timeFrame="Nov 2012 - Sept 2018"
+            company={Paycor()}
+            list={SD2List()}
+          ></JobCards>
+          <JobCards
+            name="Software Developer I"
+            timeFrame="June 2011 - Nov 2012"
+            company={Paycor()}
+            list={SD1List()}
+          ></JobCards>
+          <JobCards
+            name="Quality Assurance Analyst"
+            timeFrame="Apr 2011 - Jun 2011"
+            company={Sundries()}
+            list={QAAList()}
+          ></JobCards>
+          <JobCards
+            name="Teaching Assistant"
+            timeFrame="Nov 2010 - Jun 2011"
+            company={OU()}
+            list={TAList()}
+          ></JobCards>
+          <JobCards
+            name="Intern"
+            timeFrame="Nov 2010 - Jun 2011"
+            company={EdMap()}
+            list={EdMapList()}
+          ></JobCards>
+        </div>
       </div>
-    </div >;
+    );
   }
 
   renderSkillsList() {
-    return <div className="resumeRow">
-      <h2>Skills and Proficiencies</h2>
-      <div className="skillset">
-        <h5>Technologies</h5>
-        <li className="item">C#</li>
-        <li className="item">.NET Framework</li>
-        <li className="item">.NET Core</li>
-        <li className="item">MSSQL</li>
-        <li className="item">MVC</li>
-        <li className="item">WebAPI</li>
-        <li className="item">Restful Services</li>
-        <li className="item">Entity Framework</li>
-        <li className="item">EF Core</li>
-        <li className="item">Azure SQL</li>
-        <li className="item">Azure Storage</li>
-        <li className="item">Azure Functions</li>
-        <li className="item">React</li>
-        <h5>Tools</h5>
-        <li className="item">Visual Studio</li>
-        <li className="item">Visual Studio Code</li>
-        <li className="item">Git</li>
-        <li className="item">GitHub</li>
-        <li className="item">TFS</li>
-        <li className="item">TFS Source Control</li>
-        <li className="item">Rider</li>
-        <li className="item">TeamCity</li>
-        <li className="item">Octopus Deploy</li>
-        <li className="item">Application Insights</li>
-        <li className="item">NewRelic</li>
-        <li className="item">Azure</li>
-        <h5>Methodologies</h5>
-        <li className="item">Scrum</li>
-        <li className="item">Kanban</li>
-        <li className="item">Waterfall</li>
+    return (
+      <div className="resumeRow">
+        <h2>Skills and Proficiencies</h2>
+        <div className="skillset">
+          <h5>Technologies</h5>
+          <li className="item">C#</li>
+          <li className="item">.NET Framework</li>
+          <li className="item">.NET Core</li>
+          <li className="item">MSSQL</li>
+          <li className="item">MVC</li>
+          <li className="item">WebAPI</li>
+          <li className="item">Restful Services</li>
+          <li className="item">Entity Framework</li>
+          <li className="item">EF Core</li>
+          <li className="item">Azure SQL</li>
+          <li className="item">Azure Storage</li>
+          <li className="item">Azure Functions</li>
+          <li className="item">React</li>
+          <h5>Tools</h5>
+          <li className="item">Visual Studio</li>
+          <li className="item">Visual Studio Code</li>
+          <li className="item">Git</li>
+          <li className="item">GitHub</li>
+          <li className="item">TFS</li>
+          <li className="item">TFS Source Control</li>
+          <li className="item">Rider</li>
+          <li className="item">TeamCity</li>
+          <li className="item">Octopus Deploy</li>
+          <li className="item">Application Insights</li>
+          <li className="item">NewRelic</li>
+          <li className="item">Azure</li>
+          <h5>Methodologies</h5>
+          <li className="item">Scrum</li>
+          <li className="item">Kanban</li>
+          <li className="item">Waterfall</li>
+        </div>
       </div>
-    </div>
+    );
   }
 
   renderEducation() {
-    return <div className="resumeRow">
-      <h2>Education</h2>
-      <dl>
-        <dt>BBA with focus in Management Information Systems and Finance</dt>
-        <dd>Ohio University - <i>2007-2011 | Athens, OH</i></dd>
-      </dl>
-    </div>
+    return (
+      <div className="resumeRow">
+        <h2>Education</h2>
+        <dl>
+          <dt>BBA with focus in Management Information Systems and Finance</dt>
+          <dd>
+            Ohio University - <i>2007-2011 | Athens, OH</i>
+          </dd>
+        </dl>
+      </div>
+    );
   }
 
   renderCerts() {
-    return <div className="resumeRow">
-      <h2>Certifications</h2>
-      <div data-iframe-width="150" data-iframe-height="270" data-share-badge-id="eb715d79-8933-4a5c-bf3a-8b1d6dbb7f6e" data-share-badge-host="https://www.youracclaim.com"></div>
-    </div>
+    return (
+      <div className="resumeRow">
+        <h2>Certifications</h2>
+        <div
+          data-iframe-width="150"
+          data-iframe-height="270"
+          data-share-badge-id="eb715d79-8933-4a5c-bf3a-8b1d6dbb7f6e"
+          data-share-badge-host="https://www.youracclaim.com"
+        ></div>
+      </div>
+    );
   }
 
   render() {
@@ -178,17 +271,23 @@ export default class App extends Component {
           <h1>Justin Montgomery</h1>
           <img src={logo} className="App-logo" alt="logo" />
           <span>
-            <a className="App-link" href="mailto:justin@montgomery.run">justin@montgomery.run</a>
+            <a className="App-link" href="mailto:justin@montgomery.run">
+              justin@montgomery.run
+            </a>
           </span>
           <div>
             <span className="Icon">
-              <a href="https://www.linkedin.com/in/justinmmontgomery/"><i className="fab fa-linkedin-in"></i></a>
+              <a href="https://www.linkedin.com/in/justinmmontgomery/">
+                <i className="fab fa-linkedin-in"></i>
+              </a>
             </span>
             <span className="Icon">
-              <a href="https://www.strava.com/athletes/18781585"><i className="fab fa-strava"></i></a>
+              <a href="https://www.strava.com/athletes/18781585">
+                <i className="fab fa-strava"></i>
+              </a>
             </span>
           </div>
-        </header >
+        </header>
         <br />
         {this.renderHistoryList()}
         <br />
@@ -197,7 +296,7 @@ export default class App extends Component {
         {this.renderEducation()}
         <br />
         {this.renderCerts()}
-      </div >
+      </div>
     );
   }
 }
--- src/JobCards.js
+++ src/JobCards.js
@@ -1,30 +1,29 @@
-import React, { useState } from 'react';
-import Card from 'react-bootstrap/Card';
-import Collapse from 'react-bootstrap/Collapse';
-
+import React, { useState } from "react";
+import Card from "react-bootstrap/Card";
+import Collapse from "react-bootstrap/Collapse";
 
 function JobCards(props) {
-    const [open, setOpen] = useState(false);
-    return (
-        <>
-            <Card>
-                <Card.Header onClick={() => setOpen(!open)}>
-                    <button className="btn btn-link" type="button">{props.name}</button>
-                </Card.Header>
-                <Collapse in={open}>
-                    <div>
-                        <Card.Body>
-                            <Card.Title>
-                                {props.company}
-                            </Card.Title>
-                            <Card.Subtitle>{props.timeFrame}</Card.Subtitle>
-                            {props.list}
-                        </Card.Body>
-                    </div>
-                </Collapse>
-            </Card>
-        </>
-    );
+  const [open, setOpen] = useState(false);
+  return (
+    <>
+      <Card>
+        <Card.Header onClick={() => setOpen(!open)}>
+          <button className="btn btn-link" type="button">
+            {props.name}
+          </button>
+        </Card.Header>
+        <Collapse in={open}>
+          <div>
+            <Card.Body>
+              <Card.Title>{props.company}</Card.Title>
+              <Card.Subtitle>{props.timeFrame}</Card.Subtitle>
+              {props.list}
+            </Card.Body>
+          </div>
+        </Collapse>
+      </Card>
+    </>
+  );
 }
 
 export default JobCards;
