355915721bafc6270c833018ad1352bf5ddef79d
Justin Montgomery <justin@montgomery.run>
2021-01-24 20:51:32 -0500

    Add click on logo to call cat as a service

---
+++ src/catAsAServiceLogo.js
@@ -0,0 +1,39 @@
+import React, { useState } from "react";
+import logo from "./logo.jpg";
+import { Async } from 'react-async';
+
+function CatAsAService(props) {
+
+  const [click, setClicked] = useState(false);
+  const loadUsers = () =>
+    fetch("https://cataas.com/cat?json=true")
+      .then(res => (res.ok ? res : Promise.reject(res)))
+      .then(res => res.json())
+  if (click) {
+    return (
+      <div>
+        <Async promiseFn={loadUsers}>
+          <Async.Loading><img src={logo} className="App-logo" alt="logo" /></Async.Loading>
+          <Async.Fulfilled>
+            {data => {
+              let url = `https://cataas.com/${data.url}`;
+              return (
+                <div>
+                  <div onClick={() => setClicked(!click)}> <img src={url} className="App-logo" alt="catLogo" /></div>
+                </div>
+              )
+            }}
+          </Async.Fulfilled>
+          <Async.Rejected>
+            {error => `Something went wrong: ${error.message}`}
+          </Async.Rejected>
+        </Async>
+      </div>
+    );
+  }
+  else {
+    return <div onClick={() => { setClicked(!click) }}> <img src={logo} className="App-logo" alt="logo" /></div>
+  }
+}
+
+export default CatAsAService;
