f81d4bff845ae9d04fbbef213c1d0dcee4ff0077
Justin <justin@montgomery.run>
2021-06-03 08:56:28 -0400

    remove certs since they are broken on phone, fix later

--- src/Lists/EducationCerts.js
+++ src/Lists/EducationCerts.js
@@ -14,26 +14,11 @@ function renderEducation() {
   );
 }
 
-function renderCerts() {
-  return (
-    <div className="resumeRow">
-      <h2>Certifications</h2>
-      <div
-        data-iframe-width="150"
-        data-iframe-height="270"
-        data-share-badge-id="eb715d79-8933-4a5c-bf3a-8b1d6dbb7f6e"
-        data-share-badge-host="https://www.youracclaim.com"
-      ></div>
-    </div>
-  );
-}
-
 function educationAndCerts() {
   return (
     <div>
       {renderEducation()}
       <br />
-      {renderCerts()}
     </div>
   );
 }
