08461a198ad4872fb9ca89493028dc39cde524f8
Justin Montgomery <justin@montgomery.run>
2024-04-02 17:39:05 -0700

    Convert to ts and bun

---
+++ src/Education.tsx
@@ -0,0 +1,15 @@
+export function Education() {
+  return (
+    <>
+      <div className="resumeRow">
+        <h2>Education</h2>
+        <dl>
+          <dt>BBA with focus in Management Information Systems and Finance</dt>
+          <dd>
+            Ohio University - <i>2007-2011 | Athens, OH</i>
+          </dd>
+        </dl>
+      </div>
+    </>
+  );
+}
