b9a0b08b48b95f958e0609c3782bd439634b460b
Justin Montgomery <justin@montgomery.run>
2024-04-02 17:59:39 -0700

    Add test for rendering app

---
+++ src/App.test.tsx
@@ -0,0 +1,10 @@
+import App from './App';
+import { test } from 'bun:test';
+import { createRoot } from 'react-dom/client';
+
+test('renders without crashing', () => {
+    const div = document.createElement('div');
+    const root = createRoot(div);
+    root.render(<App />);
+    root.unmount();
+});
